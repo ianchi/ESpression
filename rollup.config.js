4cd831275cee2427f4530c0f1e1793aa463e6020
Adrian Panella <ianchi74@outlook.com>
2018-01-26 15:21:26 -0600

    chore: switch to rollup for bundling

---
+++ rollup.config.js
@@ -0,0 +1,54 @@
+import typescript from 'rollup-plugin-typescript2';
+import resolve from 'rollup-plugin-node-resolve';
+import uglify from 'rollup-plugin-uglify';
+import pkg from './package.json';
+
+import path from 'path';
+
+const MAIN_FILE = 'src/main.ts'
+
+export default [
+  {
+    input: MAIN_FILE,
+    output: {
+      name: pkg.name,
+      file: path.join(path.dirname(pkg.browser), path.basename(pkg.browser, '.js') + '.min.js'),
+      format: 'umd',
+      sourcemap: true
+    },
+    plugins: [
+      resolve(),
+      uglify(),
+      typescript(),
+    ]
+  },
+
+  {
+    input: MAIN_FILE,
+    output: {
+      name: pkg.name,
+      file: pkg.browser,
+      format: 'umd',
+      sourcemap: true
+    },
+    plugins: [
+      resolve(),
+      typescript({
+        useTsconfigDeclarationDir: true,
+        tsconfigOverride: {
+          compilerOptions: { declaration: true, declarationDir: path.dirname(pkg.types) }
+        }
+      }),
+    ]
+  },
+
+  {
+    input: MAIN_FILE,
+    external: 'tslib',
+    output: [
+      { file: pkg.main, format: 'cjs', sourcemap: true },
+      { file: pkg.module, format: 'es', sourcemap: true }
+    ],
+    plugins: [typescript()]
+  }
+];
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-export { jsepParserFactory } from './presets/jsep';
-export { es5ParserFactory } from './presets/es5';
+export { StaticEval } from './eval/eval';
+export { Parser } from './parser';
+
+export { jsepParserFactory, jsepRules } from './presets/jsep';
+export { es5ParserFactory, es5Rules } from './presets/es5';
 export { jsonPathParserFactory, es5PathParserFactory } from './presets/jsonPath';
 export { es5EvalFactory } from './eval/es5';
 export { jsonPathEvalFactory, jsonPathFactory } from './eval/jsonPathRules';
