ccf9408d9b680273ec6e35e973cc23e2debdc935
Adrian Panella <ianchi74@outlook.com>
2018-02-17 12:55:31 -0600

    fix: allow direct import from 'espression/rules'

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,5 @@ export { es5ParserFactory, es5Rules } from './presets/es5';
 export { jsonPathParserFactory, es5PathParserFactory } from './presets/jsonPath';
 export { es5EvalFactory } from './eval/es5';
 export { jsonPathEvalFactory, jsonPathFactory } from './eval/jsonPathRules';
+
+export * from './rules';
