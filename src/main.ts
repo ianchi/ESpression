54d41e5c470d0fdf17e6c6e3a07645550f4ce1df
Adrian Panella <ianchi74@outlook.com>
2018-07-13 19:26:22 -0500

    fix: add various missing exports

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,15 @@ export { jsonPathParserFactory, es5PathParserFactory } from './presets/jsonPath'
 export { es5EvalFactory, ES5StaticEval, unsuportedError } from './eval/es5';
 export { jsonPathEvalFactory, jsonPathFactory } from './eval/jsonPathRules';
 
-export { reactiveEvalFactory } from './eval/reactive';
+export { reactiveEvalFactory, ReactiveEval } from './eval/reactive';
+export * from './eval/combineMixed';
 
 export * from './rules';
+
+export * from './parser.interface';
+
+export {
+  IDENTIFIER_EXP, LITERAL_EXP, MEMBER_EXP, BINARY_EXP, ASSIGN_EXP, UPDATE_EXP, UNARY_EXP
+} from './presets/es5conf';
+
+export * from './eval/rxobject';
