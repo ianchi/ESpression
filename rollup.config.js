380c9eb5c374a6a80b8e9475cbef9209373ad2e4
Adrian Panella <ianchi74@outlook.com>
2018-08-15 19:57:23 -0500

    build: various changes
    + updated dependencies
    + use prettier as formatter
    + update linting rules

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
 import typescript from 'rollup-plugin-typescript2';
 import resolve from 'rollup-plugin-node-resolve';
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
 import path from 'path';
 
-const MAIN_FILE = 'src/main.ts'
+const MAIN_FILE = 'src/main.ts';
 
 export default [
   {
@@ -14,51 +14,64 @@ export default [
       name: pkg.name,
       file: path.join(path.dirname(pkg.browser), path.basename(pkg.browser, '.js') + '.min.js'),
       format: 'umd',
-      sourcemap: true
+      sourcemap: true,
     },
-    plugins: [
-      resolve(),
-      uglify(),
-      typescript(),
-    ]
+    plugins: [resolve(), uglify(), typescript()],
   },
-
   {
     input: MAIN_FILE,
     output: {
       name: pkg.name,
       file: pkg.browser,
       format: 'umd',
-      sourcemap: true
+      sourcemap: true,
     },
     plugins: [
       resolve(),
       typescript({
         useTsconfigDeclarationDir: true,
         tsconfigOverride: {
-          compilerOptions: { declaration: true, declarationDir: path.dirname(pkg.types) }
-        }
+          compilerOptions: {
+            declaration: true,
+            declarationDir: path.dirname(pkg.types),
+          },
+        },
       }),
-    ]
-  },
-
-  {
-    input: MAIN_FILE,
-    external: ['tslib', 'rxjs', 'rxjs/operators'],
-    output: [
-      { file: pkg.main, format: 'cjs', sourcemap: true },
-      { file: pkg.module, format: 'es', sourcemap: true }
     ],
-    plugins: [typescript()]
   },
 
   {
     input: MAIN_FILE,
-    external: ['rxjs', 'rxjs/operators'],
-    output: { file: pkg.es2015, format: 'es', sourcemap: true },
-    plugins: [typescript({
-      tsconfigOverride: {
-        compilerOptions: { target: "es2015" }
-      }})]
-  }
+    external: ['tslib'],
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+    plugins: [typescript()],
+  },
+
+  {
+    input: MAIN_FILE,
+    external: [],
+    output: {
+      file: pkg.es2015,
+      format: 'es',
+      sourcemap: true,
+    },
+    plugins: [
+      typescript({
+        tsconfigOverride: {
+          compilerOptions: { target: 'es2015' },
+        },
+      }),
+    ],
+  },
 ];
