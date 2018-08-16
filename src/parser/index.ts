e595a08c5b6fae51a9cf6eb744dcddc930996b21
Adrian Panella <ianchi74@outlook.com>
2018-08-15 20:09:01 -0500

    lint: fix missing header

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,3 +1,10 @@
+/*!
+ * Copyright (c) 2018 Adrian Panella <ianchi74@outlook.com>
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
 export * from './parser';
 export * from './parseError';
 export * from './parser.interface';
