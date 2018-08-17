f4d74962611236e5134683232f55240202689550
Adrian Panella <ianchi74@outlook.com>
2018-08-16 20:36:49 -0500

    build: remove umd bundle and 'browser' property
    This conflicts with webpack module order

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,4 @@
 import typescript from 'rollup-plugin-typescript2';
-import resolve from 'rollup-plugin-node-resolve';
-import { uglify } from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
 import path from 'path';
@@ -8,38 +6,6 @@ import path from 'path';
 const MAIN_FILE = 'src/main.ts';
 
 export default [
-  {
-    input: MAIN_FILE,
-    output: {
-      name: pkg.name,
-      file: path.join(path.dirname(pkg.browser), path.basename(pkg.browser, '.js') + '.min.js'),
-      format: 'umd',
-      sourcemap: true,
-    },
-    plugins: [resolve(), uglify(), typescript()],
-  },
-  {
-    input: MAIN_FILE,
-    output: {
-      name: pkg.name,
-      file: pkg.browser,
-      format: 'umd',
-      sourcemap: true,
-    },
-    plugins: [
-      resolve(),
-      typescript({
-        useTsconfigDeclarationDir: true,
-        tsconfigOverride: {
-          compilerOptions: {
-            declaration: true,
-            declarationDir: path.dirname(pkg.types),
-          },
-        },
-      }),
-    ],
-  },
-
   {
     input: MAIN_FILE,
     external: ['tslib'],
@@ -68,8 +34,13 @@ export default [
     },
     plugins: [
       typescript({
+        useTsconfigDeclarationDir: true,
         tsconfigOverride: {
-          compilerOptions: { target: 'es2015' },
+          compilerOptions: {
+            target: 'es2015',
+            declaration: true,
+            declarationDir: path.dirname(pkg.types),
+          },
         },
       }),
     ],
