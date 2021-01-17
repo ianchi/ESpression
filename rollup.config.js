c4925fbc4b0a32c7486c5bda043568b348b3a996
Adrian Panella <ianchi74@outlook.com>
2021-01-16 19:01:15 -0600

    chore: publish module in ES2017

--- rollup.config.js
+++ rollup.config.js
@@ -15,11 +15,6 @@ export default [
         format: 'cjs',
         sourcemap: true,
       },
-      {
-        file: pkg.module,
-        format: 'es',
-        sourcemap: true,
-      },
     ],
     plugins: [typescript()],
   },
@@ -37,7 +32,7 @@ export default [
         useTsconfigDeclarationDir: true,
         tsconfigOverride: {
           compilerOptions: {
-            target: 'es2015',
+            target: 'es2017',
             declaration: true,
             declarationDir: path.dirname(pkg.types),
           },
